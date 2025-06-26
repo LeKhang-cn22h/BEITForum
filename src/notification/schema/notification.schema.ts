e20c5cc6855682955c16e0a47c116263d77d962f
MaiKhoa0101 <maikhoa2015@gmail.com>
2025-06-26 07:34:31 +0700

    add some feature in noti, make state for is read, change some data in noti

--- src/notification/schema/notification.schema.ts
+++ src/notification/schema/notification.schema.ts
@@ -5,16 +5,20 @@ export type NotificationDocument = Notification & Document;
 
 @Schema({ timestamps: true })
 export class Notification {
-  @Prop()
-  fcmToken: Types.ObjectId;
   @Prop({ required: true })
   title: string;
+
+  @Prop()
+  userReceiveNotificationId: Types.ObjectId; // ✅ Đúng cách định nghĩa với @Prop
+
   @Prop({ required: true })
   content: string;
+
   @Prop({ type: Types.ObjectId, ref: 'posts', required: true })
   postId: Types.ObjectId;
-  createdAt: Date;
-  updatedAt: Date;
+
+  @Prop({ default: false })
+  isRead: boolean; // ✅ Đúng cách định nghĩa với @Prop
 }
 
 export const NotificationSchema = SchemaFactory.createForClass(Notification);
